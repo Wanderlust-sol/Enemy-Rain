02719a57169824315aa748aac08343b1ff24fdb4
Taesol Kwon <ds3bbbb@naver.com>
2020-02-09 19:43:20 +0900

    final

--- enemy.js
+++ enemy.js
@@ -1,3 +1,68 @@
 class Enemy {
-  
+  constructor() {
+    this.bgForm = document.querySelector("#bg");
+
+    this.winHero = document.querySelector(".hero");
+    this.createGhost();
+  }
+
+  createGhost = () => {
+    this.createSpan = document.createElement("span");
+    this.randomNum = Math.floor(Math.random() * 955);
+    this.createSpan.className += "enemy-image";
+    this.bgForm.appendChild(this.createSpan);
+    this.createSpan.style.left = this.randomNum + "px";
+    this.falling();
+  };
+
+  falling = () => {
+    setInterval(() => {
+      const topValue = window.getComputedStyle(this.createSpan).top;
+      const enemyOffsetTop = this.createSpan.offsetTop;
+      const enemyOffsetLeft = this.createSpan.offsetLeft;
+      const heroOffsetTop = this.winHero.offsetTop;
+      const heroOffsetLeft = this.winHero.offsetLeft;
+
+      this.createSpan.style.top = parseInt(enemyOffsetTop) + 1 + "px";
+
+      if (
+        heroOffsetTop == enemyOffsetTop + 53 &&
+        heroOffsetLeft >= enemyOffsetLeft - 35 &&
+        heroOffsetLeft <= enemyOffsetLeft + 35
+      ) {
+        this.createSpan.classList.add("kill-enemy");
+        const sound = new Audio("audio/dying.wav");
+        sound.play();
+      }
+
+      if (this.createSpan.style.top === "700px") {
+        this.createSpan.remove();
+      }
+    }, 1);
+  };
 }
+
+let intervalMaking;
+
+function startGhost() {
+  const startBtn = document.querySelector(".start");
+  startBtn.addEventListener("click", function() {
+    intervalMaking = setInterval(() => {
+      const newEnemy = new Enemy();
+    }, 3000);
+  });
+}
+
+function stopGhost() {
+  const stopBtn = document.querySelector(".stop");
+  stopBtn.addEventListener("click", function() {
+    clearInterval(intervalMaking);
+  });
+}
+
+function init() {
+  startGhost();
+  stopGhost();
+}
+
+init();
--- hero.js
+++ hero.js
@@ -1,3 +1,59 @@
-class Hero {
-  
+// class Hero {
+//   constructor() {
+//     this.indexHero = document.querySelector(".hero");
+//     this.leftCss = window.getComputedStyle(indexHero).left;
+//     this.leftSide = "0px";
+//     this.rightSide = "960px";
+//   }
+// }
+
+const indexHero = document.querySelector(".hero");
+
+function frontFace1(event) {
+  indexHero.classList.remove("left-face");
 }
+
+function frontFace2(event) {
+  indexHero.classList.remove("right-face");
+}
+
+function leftKey(x) {
+  const leftCss = window.getComputedStyle(indexHero).left;
+  const leftSide = "0px";
+
+  indexHero.classList.add("left-face");
+  indexHero.style.left = parseInt(leftCss) - 20 + "px";
+  //indexHero.style.left = parseInt(indexHero.style.left) - 20 + "px";
+  window.addEventListener("keyup", frontFace1);
+
+  if (leftCss === leftSide) {
+    indexHero.style.left = "0px";
+  }
+}
+
+function rightKey() {
+  const leftCss = window.getComputedStyle(indexHero).left;
+  const rightSide = "960px";
+
+  indexHero.classList.add("right-face");
+  indexHero.style.left = parseInt(leftCss) + 20 + "px";
+  window.addEventListener("keyup", frontFace2);
+
+  if (leftCss === rightSide) {
+    indexHero.style.left = "960px";
+  }
+}
+
+function pressKey(e) {
+  if (e.keyCode == 37) {
+    leftKey();
+  } else if (e.keyCode == 39) {
+    rightKey();
+  }
+}
+
+function movingCon() {
+  window.addEventListener("keydown", pressKey);
+}
+
+movingCon();
